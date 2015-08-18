7de98d3f3141770240403f6fab4eb5c699755134
Saeed Esmaeili <smaeily@gmail.com>
2015-08-19 01:54:16 +0430

    Archive Item ViewModel , used in Archive PartialView ShowArchive action in BlogPosts Controller

---
+++ Models/ArchiveItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class ArchiveItemViewModel
+    {
+        public int Year { get; set; }
+        public int Mounth { get; set; }
+    }
+}
