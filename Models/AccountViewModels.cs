7eb46da80b677137af93441263726a366f8b9d21
Saeed Esmaeili <smaeily@gmail.com>
2015-08-22 18:42:46 +0430

    DisplayName Updated

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -5,8 +5,8 @@ namespace Blog.Models
 {
     public class ExternalLoginConfirmationViewModel
     {
-        [Required]
-        [Display(Name = "Email")]
+        [Required(ErrorMessage ="وارد کردن {0} الزامیست")]
+        [Display(Name = "ایمیل")]
         public string Email { get; set; }
     }
 
@@ -25,15 +25,15 @@ namespace Blog.Models
 
     public class VerifyCodeViewModel
     {
-        [Required]
+        [Required(ErrorMessage ="وارد کردن {0} الزامیست")]
         public string Provider { get; set; }
 
-        [Required]
-        [Display(Name = "Code")]
+        [Required(ErrorMessage ="وارد کردن {0} الزامیست")]
+        [Display(Name = "کد")]
         public string Code { get; set; }
         public string ReturnUrl { get; set; }
 
-        [Display(Name = "Remember this browser?")]
+        [Display(Name = "این مرورگر را به خاطر بسپارم ؟")]
         public bool RememberBrowser { get; set; }
 
         public bool RememberMe { get; set; }
@@ -41,70 +41,70 @@ namespace Blog.Models
 
     public class ForgotViewModel
     {
-        [Required]
-        [Display(Name = "Email")]
+        [Required(ErrorMessage ="وارد کردن {0} الزامیست")]
+        [Display(Name = "ایمیل")]
         public string Email { get; set; }
     }
 
     public class LoginViewModel
     {
-        [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
+        [Required(ErrorMessage ="وارد کردن {0} الزامیست")]
+        [Display(Name = "ایمیل")]
+        [EmailAddress(ErrorMessage ="ایمیل وارد شده معتبر نیست")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="وارد کردن {0} الزامیست")]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "رمز عبور")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "به خاطر بسپارم ؟")]
         public bool RememberMe { get; set; }
     }
 
     public class RegisterViewModel
     {
-        [Required]
-        [Display(Name = "FirstName")]
+        [Required(ErrorMessage ="وارد کردن {0} الزامیست")]
+        [Display(Name = "نام")]
         public string FirstName { get; set; }
 
-        [Required]
-        [Display(Name = "LastName")]
+        [Required(ErrorMessage ="وارد کردن {0} الزامیست")]
+        [Display(Name = "نام خانوادگی")]
         public string LastName { get; set; }
 
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
+        [Required(ErrorMessage ="وارد کردن {0} الزامیست")]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نیست")]
+        [Display(Name = "ایمیل")]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage ="وارد کردن {0} الزامیست")]
+        [StringLength(100, ErrorMessage = "{0} حداقل باید {2} کاراکتر باشد.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "رمزعبور")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "تکرار رمزعبور")]
+        [Compare("Password", ErrorMessage = "کلمه عبور و تکرار آن یکی نیستند.")]
         public string ConfirmPassword { get; set; }
     }
 
     public class ResetPasswordViewModel
     {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
+        [Required(ErrorMessage ="وارد کردن {0} الزامیست")]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نیست")]
+        [Display(Name = "ایمیل")]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage ="وارد کردن {0} الزامیست")]
+        [StringLength(100, ErrorMessage = "{0} حداقل باید {2} کاراکتر باشد.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "رمزعبور")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "تکراررمزعبور")]
+        [Compare("Password", ErrorMessage = "کلمه عبور و تکرار آن یکی نیستند.")]
         public string ConfirmPassword { get; set; }
 
         public string Code { get; set; }
@@ -112,9 +112,9 @@ namespace Blog.Models
 
     public class ForgotPasswordViewModel
     {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
+        [Required(ErrorMessage ="وارد کردن {0} الزامیست")]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نیست")]
+        [Display(Name = "ایمیل")]
         public string Email { get; set; }
     }
 }
