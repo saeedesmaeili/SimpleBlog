b3d71191b31acef99fcbd2cea22c9fc7d012f743
Saeed Esmaeili <smaeily@gmail.com>
2015-08-26 10:28:23 +0430

    Add Jquery LazyLoading

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -28,7 +28,8 @@ namespace Blog
                       "~/Scripts/syntaxhighlighter_3.0.83/scripts/shBrushSql.js",
                       "~/Scripts/syntaxhighlighter_3.0.83/scripts/shBrushXml.js",
                       "~/Scripts/syntaxhighlighter_3.0.83/scripts/shBrushPowerShell.js",
-                      "~/Scripts/syntaxhighlighter_3.0.83/scripts/shBrushCss.js"
+                      "~/Scripts/syntaxhighlighter_3.0.83/scripts/shBrushCss.js",
+                      "~/Scripts/jquery.lazyload.js"
                       ));
 
             bundles.Add(new ScriptBundle("~/bundles/admin").Include(
