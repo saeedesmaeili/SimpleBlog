7f58d25d86dbf723e79b978ce28d504874cdde6a
Saeed Esmaeili <smaeily@gmail.com>
2015-08-26 16:58:19 +0430

    Update Display Names

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -13,12 +14,20 @@ namespace Blog.Models
         }
         public Guid Id { get; set; }
 
+        [Required]
+        [Display(Name = "نام شاخه")]
         public string Name { get; set; }
 
+        [Required]
+        [Display(Name = "Icon")]
         public string IconClass { get; set; }
 
+        [Required]
+        [Display(Name = "اولویت چیدمان")]
         public int Order { get; set; }
 
+        [Required]
+        [Display(Name = "شاخه پدر")]
         public Guid ParentId { get; set; }
 
         public virtual ICollection<BlogPost> BlogPosts { get; set; }
