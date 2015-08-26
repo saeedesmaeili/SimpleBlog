b0c6ff3034fd623a5e4a3929dd19f672aef6dc24
Saeed Esmaeili <smaeily@gmail.com>
2015-08-26 17:25:44 +0430

    Add Display Name

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -13,7 +14,7 @@ namespace Blog.Models
         }
         public Guid Id { get; set; }
 
-        //[Ds]
+        [Required , Display(Name ="عنوان")]
         public string Title { get; set; }
 
         public virtual ICollection<BlogPost> BlogPosts { get; set; }
